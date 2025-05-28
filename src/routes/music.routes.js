75595b757ef4705f913684cc4efa721b4e8a888d
Gabriel-zet <bieloliveira4567jk@gmail.com>
2025-05-27 22:56:34 -0300

    feat: adciona rota para buscar musica por id

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');  
 const router = express.Router();  
 const { searchMusic } = require('../controllers/music.controller');  
+const { searchMusicById } = require('../controllers/uniqueMusic.controller');
 
 router.get('/search', searchMusic);  
+router.get('/unique/:id', searchMusicById)
 
 module.exports = router;  
